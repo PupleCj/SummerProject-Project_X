0c8a49609964f5ec05d4daae7d893d4da9ed7d1a
CodeName-Sudo <kieranhourigan4@gmail.com>
2025-03-26 12:37:41 +0000

    Update JRPGCombatManager.cpp

--- SummerProject_PX/Content/_Kieran/JRPGCombatManager.cpp
+++ SummerProject_PX/Content/_Kieran/JRPGCombatManager.cpp
@@ -1,1 +1,60 @@
+#include "JRPGCombatManager.h"
+#include "JRPGCharacter.h"
 
+AJRPGCombatManager::AJRPGCombatManager()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    CurrentTurnIndex = 0;
+}
+
+void AJRPGCombatManager::BeginPlay()
+{
+    Super::BeginPlay();
+    DetermineTurnOrder();
+}
+
+void AJRPGCombatManager::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (!IsCombatOver())
+    {
+        HandleTurn();
+    }
+}
+
+void AJRPGCombatManager::StartCombat()
+{
+    //initialize combat here
+    DetermineTurnOrder();
+}
+
+void AJRPGCombatManager::EndCombat()
+{
+    // Cleanup after combat
+}
+
+void AJRPGCombatManager::HandleTurn()
+{
+    if (CurrentCharacter)
+    {
+        //handle the current character's turn
+        // ...
+
+        //move to the next character's turn
+        CurrentTurnIndex = (CurrentTurnIndex + 1) % (PlayerCharacters.Num() + EnemyCharacters.Num());
+        CurrentCharacter = (CurrentTurnIndex < PlayerCharacters.Num()) ? PlayerCharacters[CurrentTurnIndex] : EnemyCharacters[CurrentTurnIndex - PlayerCharacters.Num()];
+    }
+}
+
+void AJRPGCombatManager::DetermineTurnOrder()
+{
+    //determine the turn order based on character speed or other stats
+    
+}
+
+bool AJRPGCombatManager::IsCombatOver()
+{
+    //check if all enemies or all players are defeated
+    return EnemyCharacters.Num() == 0 || PlayerCharacters.Num() == 0;
+}
