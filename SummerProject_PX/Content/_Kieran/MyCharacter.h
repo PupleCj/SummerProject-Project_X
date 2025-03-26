3ae68460888e77c07ad0d5fc939a15bfd16e4f3f
CodeName-Sudo <kieranhourigan4@gmail.com>
2025-03-26 12:38:55 +0000

    Create MyCharacter.h

---
+++ SummerProject_PX/Content/_Kieran/MyCharacter.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class ECharacterState : uint8
+{
+    ECS_Exploration UMETA(DisplayName = "Exploration"),
+    ECS_Combat UMETA(DisplayName = "Combat")
+};
+
+UCLASS()
+class YOURGAME_API AMyCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AMyCharacter();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void TurnAtRate(float Rate);
+    void LookUpAtRate(float Rate);
+    void StartSprinting();
+    void StopSprinting();
+    void StartCombat();
+    void EndCombat();
+    void Attack();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+    float BaseTurnRate;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+    float BaseLookUpRate;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float WalkSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float SprintSpeed;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", meta = (AllowPrivateAccess = "true"))
+    ECharacterState CharacterState;
+
+    void SwitchToCombatMode();
+    void SwitchToExplorationMode();
+};
